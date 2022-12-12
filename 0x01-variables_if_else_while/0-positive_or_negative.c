dace709928cc86592c9e6f9617289120c8e9734e
GreywiseNoface <111127441+GreywiseNoface@users.noreply.github.com>
2022-12-12 21:47:19 +0200

    Betty-style enhancement to code.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,59 +4,51 @@
 
 #include <time.h>
 
-extern int grey;
+typedef int grey;
 
 
-
-/* 
+/**
+ *Program prints the interger and states whether it is positive or negative
  *
- * * Program prints the interger and states whether it is positive or negative
- *
- * * 
- *
- * * Description: A random value is assigned to n. 
- *
- * * The conditional statements below print the interger and state whether
- *
- * * the value of n is positive , negative or zero /
- *
- * **/
-
+ *Description: A random value is assigned to n. 
+ *The conditional statements below print the interger and state whether
+ *the value of n is positive , negative or zero /
+ */
 
 
 int main(void)
 
 {
+	int n;
 
-		int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
+	if (n>0)
 
+		printf("%i is positive\n", n);
 
-			srand(time(0));
+	else if (n<0)
 
-				n = rand() - RAND_MAX / 2;
+		printf("%d is negative\n", n);
 
-					
+	else
 
+		printf("%i is zero\n", n);
+	
+	return (0);
 
-
-				    if (n>0)
-
-					    		printf("%i is positive\n", n);
-
-				        else if (n<0)
-
-							   printf("%d is negative\n", n);
-
-					    else
-
-						          printf("%i is zero\n", n);
+}					
 
 
 
-					    	/* your code goes there */
+				    
 
-					    	return (0);
+
+
+					    	
+
+					    	
 
 
 
