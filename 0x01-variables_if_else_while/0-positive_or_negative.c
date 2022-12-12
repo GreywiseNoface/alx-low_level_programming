42bc9f4bf965b9c24fb99487f9bc3c7a962e2758
GreywiseNoface <renegade.grey@yahoo.com>
2022-12-12 10:08:12 -0800

    Random interger code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+extern int grey;
+
+
+
+/* 
+ *
+ * * Program prints the interger and states whether it is positive or negative
+ *
+ * * 
+ *
+ * * Description: A random value is assigned to n. 
+ *
+ * * The conditional statements below print the interger and state whether
+ *
+ * * the value of n is positive , negative or zero /
+ *
+ * **/
+
+
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					
+
+
+
+				    if (n>0)
+
+					    		printf("%i is positive\n", n);
+
+				        else if (n<0)
+
+							   printf("%d is negative\n", n);
+
+					    else
+
+						          printf("%i is zero\n", n);
+
+
+
+					    	/* your code goes there */
+
+					    	return (0);
+
+
+
+}
+
+	
